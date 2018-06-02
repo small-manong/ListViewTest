bd033bf65812c39d85e10dcb7d0683b5c56cd564
李朋伟 <lpw@meitu.com>
2018-06-02 09:57:22 +0800

    jj

---
+++ app/src/main/java/com/example/listviewtest/Fruit.java
@@ -0,0 +1,22 @@
+package com.example.listviewtest;
+
+/**
+ * Created by 475 on 2018/6/2.
+ */
+public class Fruit {
+    private String name;
+    private int imageId;
+
+    public Fruit(String name, int imageId) {
+        this.name = name;
+        this.imageId = imageId;
+    }
+
+    public int getImageId() {
+        return imageId;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
